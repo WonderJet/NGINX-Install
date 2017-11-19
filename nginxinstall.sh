b2e7888632d0fc5f901754b60dde47f087d32065
Jacob Purser <jpurser21@gmail.com>
2017-11-19 14:24:39 -0700

    Update nginxinstall.sh

--- nginxinstall.sh
+++ nginxinstall.sh
@@ -1,8 +1,32 @@
 #!bin/bash
 
-#Installs and configures nginx
+domain=$1
+root="/var/www/example.com/html"
+block="/etc/nginx/sites-available/example.com"
+
+#Installs nginx
 
 su root
-apt-get update
-apt-get install nginx
+sudo apt-get update
+sudo apt-get install nginx
+
+#Configures nginx
+
+sudo mkdir -p $root
+sudo tee $block > /dev/null <<EOF
+server {
+        listen 3200;
+        
+        root /var/www/example.com/html;
+        index index.html index.htm;
+        
+        server_name example.com www.example.com;
+        
+        location ~ \.php {
+                    proxy_pass http://127.0.0.1:3400;
+        }
+}                    
+
+
+
 
